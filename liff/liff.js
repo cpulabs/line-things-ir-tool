644e8d991d1ea39d87757122fd0c1145fbb239e1
ito.takahiro <ito.takahiro@linecorp.com>
2019-01-29 16:51:14 +0900

    add liff

--- liff/liff.js
+++ liff/liff.js
@@ -3,9 +3,9 @@
 
 
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID           = 'b8718b56-80a2-4a05-a224-c35a5a9accb0'; // CMD
-const CMD_CHARACTERISTIC_UUID     = '95243321-cb66-4137-802f-4cb51fd4818d';
-const MATRIX_CHARACTERISTIC_UUID  = '943f94a6-3a7e-45df-8614-1e5f61fe334f';
+const USER_SERVICE_UUID           = 'd6d88913-6e55-4e8d-87f4-af235868b377'; // CMD
+const CMD_CHARACTERISTIC_UUID     = 'e98498a0-e624-4150-9c19-8ae5b6e7b044';
+const MATRIX_CHARACTERISTIC_UUID  = '2fe4e8a1-af17-4faa-b587-7d639a79b9c1';
 
 // PSDI Service UUID: Fixed value for Developer Trial
 const PSDI_SERVICE_UUID         = 'e625601e-9e55-4597-a598-76018a0d293d'; // Device ID
@@ -14,7 +14,7 @@ const PSDI_CHARACTERISTIC_UUID  = '26e2b12b-85f0-4f3f-9fdd-91d114270e6e';
 
 
 // Matrix data
-let g_color_matrix8x8 = [];
+let g_rawcode = [];
 
 
 
@@ -25,78 +25,7 @@ let g_color_matrix8x8 = [];
 
 window.onload = () => {
     initializeApp();
-
-    var adder_v = 0;
-    var color_matrix = [];///[144, 044, 50, 1, 32, 32, 32, 55, 89, 32, 50, 32, 2, 32, 66, 54];
-
-    for(var i = 0; i < 8; i = i + 1){
-        g_color_matrix8x8[i] = [25, 25, 25, 25, 25, 25, 25, 25];//[];
-    }
-
-    writeColorSample();
-    drawMatrix320x320();
-
-        var test1 = convertTemo2ColorHsv(0);
-            var test2 = convertTemo2ColorHsv(50);
-                var test3 = convertTemo2ColorHsv(99);
-
 };
-
-function writeColorSample(){
-  var canvas = document.getElementById('color_sample');
-  var context = canvas.getContext('2d');
-
-  var i = 0;
-  var j = 0;
-
-  for(var i = 0; i < 100; i = i + 1){
-    context.fillStyle = convertTemo2ColorHsv(i);//convertTemp2Color(i);//chroma.temperature(color_data[i][j]);
-    context.fillRect(i*2, 1, 2, 10);
-  }
-}
-
-
-function convertTemo2ColorHsv(temp){
-  var color_value = 240 - ((240 / 100) * temp);
-  var hsv_color =  chroma.hsv(color_value, 1, 1);
-  return hsv_color;
-}
-
-function convertTemp2Color(temp){
-  var magnification = 1.8;//180 / 100;    //範囲
-  var white_start = 150;
-
-  var blue;
-  if(magnification * temp < 100){
-    blue = Math.cos(3.141592/100*(magnification*temp)) * 0.5 + 0.5;   //0 ~ 1
-    blue = blue * 255
-  }else if(magnification * temp > white_start){
-    blue = (255 / (180 - white_start)) * ((magnification * temp) - white_start);
-  }else{
-    blue = 0;
-  }
-
-  var green = Math.sin(3.141592/100 * magnification * temp);
-  if(magnification * temp > white_start){
-    green = (255 / (180 - white_start)) * ((magnification * temp) - white_start);
-  }else{
-    green = green * 255;
-  }
-
-  var red;
-  if(magnification * temp > 50){
-    red = Math.cos(3.141592/100*(magnification*temp + 90)) * 0.5 + 0.5;   //0 ~ 1
-    red = red * 255
-  }else if(magnification * temp > white_start){
-    red = (255 / (180 - white_start)) * ((magnification * temp) - white_start);
-  }else{
-    red = 0;
-  }
-
-  return chroma(red, green, blue)
-
-}
-
 
 // ----------------- //
 // Handler functions //
@@ -312,40 +241,21 @@ function liffGetMatrixDataCharacteristic(characteristic) {
     characteristic.startNotifications().then(() => {
         characteristic.addEventListener('characteristicvaluechanged', e => {
             //const val = (new Uint8Array(e.target.value.buffer))[0];
-            const matrix = (new Uint8Array(e.target.value.buffer));     //16Byte
+            const data = (new Uint8Array(e.target.value.buffer));     //16Byte
 
-            //Detect Color
-            var color_matrix = [];
-            for(var i = 0; i < 16; i = i + 1){
-                color_matrix[i] = matrix[i] & 0x7f;
-            }
 
-            //Detect V-Address
-            var v_address = 0;
-            v_address = ((matrix[0] & 0x80) >> 7) + ((matrix[1] & 0x80) >> 6);
+            uiDebugMessage(data);
 
-            //Draw Color matrix
-            uiDebugMessage(matrix);
-            uiDebug1Message(color_matrix);
-            uiDebug2Message(v_address);
+            var index = (data[0] << 24) + (data[1] << 16) + (data[2] << 8) + data[3];
+            var length = (data[4] << 24) + (data[5] << 16) + (data[6] << 8) + data[7];
+            var code0 = (data[8] << 24) + (data[9] << 16) + (data[10] << 8) + data[11];
+            var code1 = (data[12] << 24) + (data[13] << 16) + (data[14] << 8) + data[15];
 
-            for(var i = 0; i < 8; i = i + 1){
-                g_color_matrix8x8[v_address*2][i] = color_matrix[i];
-                g_color_matrix8x8[v_address*2+1][i] = color_matrix[i+8];
-            }
-            if(v_address == 3){
-                drawMatrix320x320();
-                uiDebug1Message("Stop");
-                uiDebug2Message(g_color_matrix8x8);
+            g_rawcode[index*2] = code0;
+            g_rawcode[index*2 + 1] = code1;
 
-                if (document.config.get_singleshot.checked){
-                  window.cmdCharacteristic.writeValue(
-                      new Uint8Array([0x04, 0, 0, 0])
-                  ).catch(error => {
-                      uiDebugMessage("oneshot");
-                      uiStatusError(makeErrorMsg(error), false);
-                  });
-                }
+            if(index == length){
+                uiDebugMessage1(g_rawcode);
             }
 
         });
@@ -355,70 +265,6 @@ function liffGetMatrixDataCharacteristic(characteristic) {
     });
 }
 
-
-function drawMatrix320x320(){
-  var canvas_original = document.getElementById('matrix_original');
-  var context_original = canvas_original.getContext('2d');
-
-  var canvas_320x320 = document.getElementById('matrix_320x320');
-  var context_320x320 = canvas_320x320.getContext('2d');
-
-  context_320x320.drawImage(canvas_original,0,0);
-
-
-  var destination = context_320x320.createImageData(320, 320);
-  //var src_imagedata = ctx.createImageData(8, 8);
-  var src_imagedata = new ImageData(8, 8);
-
-  //最大、最低温度取得
-  var array_min = [];
-  var array_max = [];
-
-  for(i = 0; i < 8; i = i + 1){
-    array_max[i] = Math.max.apply(null, g_color_matrix8x8[i]);
-    array_min[i] = Math.min.apply(null, g_color_matrix8x8[i]);
-  }
-  var max = Math.max.apply(null, array_max);
-  var min = Math.min.apply(null, array_min);
-
-  document.getElementById("max_temp").innerText = max + "℃";
-  document.getElementById("min_temp").innerText = min + "℃";
-
-  var magnification = 1;
-  if (document.config.adj_range.checked){
-    if(max * 4 < 100){
-      magnification = 4;
-    }else if(max * 3 < 100){
-      magnification = 3;
-    }else if(max * 2 < 100){
-      magnification = 2;
-    }
-    
-  }
-
-  //Draw Matrix
-  for(var i = 0; i < 8; i = i + 1){
-    for(var j = 0; j < 8; j = j + 1){
-
-      var pixel_color = convertTemo2ColorHsv(g_color_matrix8x8[j][i] * magnification);
-
-      var red = pixel_color.get( "rgb.r" );
-      var green = pixel_color.get( "rgb.g" );
-      var blue = pixel_color.get( "rgb.b" );
-
-      src_imagedata.data[i * 32 + j * 4] = red;
-      src_imagedata.data[i * 32 + j * 4 + 1] = green;
-      src_imagedata.data[i * 32 + j * 4 + 2] = blue;
-      src_imagedata.data[i * 32 + j * 4 + 3] = 255;//alpha;
-    }
-  }
-
-  // Perform scaling (拡大縮小の実行)
-  EffectResampling(src_imagedata,destination,BiCubic_Filter,false);
-
-  // Draw to canvas (canvasへ描画)
-  context_320x320.putImageData(destination,0,0);
-}
 
 
 function liffWriteLoadMatrix() {
